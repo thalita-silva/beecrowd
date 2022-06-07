adbdc10ae6dec061200b3d10d12fb7fce7e7d50c
Thalita <thalitaaesser@gmail.com>
2022-06-07 09:57:16 -0400

    update

--- sql/2609.sql
+++ sql/2609.sql
@@ -1,5 +1,5 @@
 --author: Thalita
 --2609 - Produtos por categoria
-select name, sum(products.amount) from products
+select categories.name, sum(products.amount) from products
 join categories on categories.id = products.id_categories
 group by categories.name;
